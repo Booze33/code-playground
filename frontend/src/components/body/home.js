b3dea49410e023303d9be5ca5c10ee8cae86ec99
Booze33 <tebetisloh@gmail.com>
2023-12-23 03:20:19 -0600

    user auth

--- frontend/src/components/body/home.js
+++ frontend/src/components/body/home.js
@@ -1,7 +1,24 @@
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from '../../redux/auth/authSlice';
+
 const Home = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    dispatch(signOut());
+    setTimeout(() => {
+      navigate('/');
+    }, 2000);
+  };
+
   return(
     <div>
       <h1>Tisloh</h1>
+      <button onClick={handleSignOut} type="button">
+        Sign Out
+      </button>
     </div>
   )
 }
